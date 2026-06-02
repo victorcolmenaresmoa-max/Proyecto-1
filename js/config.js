d60e76264d91d514c0c819883fc0d9093a5f17f8
victorcolmenaresmoa-max <victorcolmenares.moa@gmail.com>
2026-06-02 14:49:42 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,7 @@
-// js/config.js
-const API_BASE = 'https://sheetdb.io/api/v1/0ezp3ubwo2fuz';
+// 1. Coloca aquí tu ID de SheetDB
+const API_BASE = 'https://sheetdb.io/api/v1/0ezp3ubwo2fuz'; 
 
-export const API_URL = `${API_BASE}?sheet=Pedidos`;
-export const API_PRODUCTOS = `${API_BASE}?sheet=Productos`;
+// 2. Exportamos las rutas exactas para cada archivo
+export const API_URL = `${API_BASE}?sheet=Pedidos`;         // Lo usa admin.js para leer
+export const API_PEDIDOS = `${API_BASE}?sheet=Pedidos`;     // Lo usa checkout.js para guardar
+export const API_PRODUCTOS = `${API_BASE}?sheet=Productos`; // Lo usa catalogo.js y admin.js
